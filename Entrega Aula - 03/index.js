6a91c699b1a2fafdb3a705c5785985ca45eb1ee4
Estheferson <estheferson01@hotmail.com>
2022-10-16 22:56:02 -0300

    Adicionando o arquivo

---
+++ Entrega Aula - 03/index.js
@@ -0,0 +1,5 @@
+const Calculadora = require('./src/calculadora');
+
+let qCalculadora = Calculadora.quadrado(5);
+
+console.log(qCalculadora);
---
+++ Entrega Aula - 03/validate.js
@@ -0,0 +1,9 @@
+const calculadoraConstraint = {
+    n1: {
+        type: 'number',
+    },
+    n2: {
+        type: 'number',
+    },
+};
+module.exports = { calculadoraConstraint };
